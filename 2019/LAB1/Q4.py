39ec5c16bc19034e8c9595d8f85f0dc63d033d9e
Muppuri Siva Jagadesh <56889104+jaggu21@users.noreply.github.com>
2019-10-30 01:15:13 +0530

    Update Q4.py

--- 2019/LAB1/Q4.py
+++ 2019/LAB1/Q4.py
@@ -1,5 +1,5 @@
-a = input()
-b = input()
+a = int (raw_input())
+b = int (raw_input())
 import math 
 d =  a**2 + b**2#calculates sum of squares
 c = pow(long(d),0.5) #calculating the third side using pythagorean theorem
